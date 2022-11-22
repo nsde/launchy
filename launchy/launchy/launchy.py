afcfc633d7f51ccb36793ee7a8292fb4ff7e1f8e
NSDE <neostyxde@gmail.com>
2022-11-22 18:55:37 +0100

    WIP

--- launchy/launchy/launchy.py
+++ launchy/launchy/launchy.py
@@ -1,4 +1,7 @@
+import os
+import uuid
 import flask
+import logos
 
 from .. import tools
 
@@ -6,8 +9,26 @@ launchy_bp = flask.Blueprint('launchy_bp', __name__, template_folder='../')
 
 @launchy_bp.route('/')
 def home():
-    return flask.render_template('launchy/templates/home.html')
+    apps = tools.read_json('config/library').items()
+    return flask.render_template('launchy/templates/home.html', apps=apps)
 
-@launchy_bp.route('/add')
+@launchy_bp.route('/add', methods=['GET', 'POST'])
 def add():
+    if flask.request.method == 'POST':
+        apps = tools.read_json('config/library')
+        form_input = flask.request.form.get
+        apps[uuid.uuid4()] = {
+            'name': form_input(''),
+            'cover': '',
+            'command': form_input('command')
+        }
+        return flask.redirect('/')
+        
     return flask.render_template('launchy/templates/add.html')
+
+@launchy_bp.route('/launch/<app_id>')
+def launch(app_id):
+    apps = tools.read_json('config/library')
+    os.system(apps[app_id]['command'])
+
+    return flask.Response(200)
